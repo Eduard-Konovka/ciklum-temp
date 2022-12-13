44270d000cd71a30f3ad7fe4b7f2a1740687e23f
Eduard Konovka <ed098ua@gmail.com>
2022-12-14 00:27:42 +0200

    Adds solution of Asynchrony3

--- Week8/Asynchrony3/index.js
+++ Week8/Asynchrony3/index.js
@@ -1,38 +1,50 @@
 async function myAsyncFunction() {
-    return 'Hello';
+  return "Hello";
 }
 
-myAsyncFunction().then(
-    function(value) {
-        console.log(value);
-    }
-)
+myAsyncFunction().then(function (value) {
+  console.log(value);
+});
+// або
+myAsyncFunction().then((value) => console.log(value));
+
+// ===========================================================================
 
 function myTimedFunction() {
-    return new Promise(resolve => {
-        setTimeout(() => resolve('Async Hello!'), 3000);
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Async Hello!"), 3000);
+  });
 }
 
 async function promiseInAction() {
-    const result = await myTimedFunction();
-    console.log(result); 
+  const result = await myTimedFunction();
+  console.log(result);
 }
 
 promiseInAction();
 
-let myErrorPromise = new Promise(function(resolve, reject) {
-   reject('От халепа');
-})
+// ===========================================================================
 
-myErrorPromise.then(
-    function(error) {
-        console.log(error.message);
-    }
-)
+let myErrorPromise1 = new Promise(function (resolve, reject) {
+  //   resolve("Ok");
+  throw new Error("Халепа!");
+});
+// або
+let myErrorPromise2 = new Promise(function (resolve, reject) {
+  //   resolve("Ok");
+  reject("От халепа...");
+});
 
-myErrorPromise.catch(
-    function(error) {
-        console.log(error.message);
-    }
-)
+myErrorPromise1.then(function (result) {
+  console.log("result 1 ---> ", result);
+});
+myErrorPromise1.catch(function (error) {
+  console.log("error 1 ---> ", error.message); // error.message
+});
+
+myErrorPromise2.then(function (result) {
+  console.log("result 2 ---> ", result);
+});
+myErrorPromise2.catch(function (error) {
+  console.log("error 2 ---> ", error); // error
+});
