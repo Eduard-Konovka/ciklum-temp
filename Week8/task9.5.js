85b06cf6d4882d9a745128fd858c83673b0615d9
Eduard Konovka <ed098ua@gmail.com>
2022-12-20 16:55:34 +0200

    Fix task9.5

--- Week8/task9.5.js
+++ Week8/task9.5.js
@@ -6,38 +6,44 @@ async function handleRequest(options) {
   }
 
   return fetch(options).then((response) => {
+    // варіант #1 -------------------------------------
+    if (response.status >= 200 && response.status < 400) {
+      return Promise.resolve(response.json());
+    } else if (response.status >= 400) {
+      throw new Error(response.status);
+    }
+
+    // варіант #2 -------------------------------------
     return new Promise((resolve, reject) => {
       if (response.status >= 200 && response.status < 400) {
         resolve(response.json());
       } else if (response.status >= 400) {
         reject(new Error(response.status));
+        // варіант #2.2 -------------------------------
+        throw new Error(response.status);
       }
     });
-
-    // if (response.status >= 200 && response.status < 400) {
-    //   return Promise.resolve(response.json());
-    // } else if (response.status >= 400) {
-    //   throw Promise.reject(new Error(response.status));
-    // }
   });
   // ========== Кінець зони редагування ===============
 }
 
-//
-
+// Код для перевірки в консолі =======================
 const visualize = async (val) => {
   try {
-    console.log("Name of pokemon ---> ", await handleRequest(val));
+    console.log(
+      "Name of pokemon ---> ",
+      await handleRequest(val).then((obj) => obj.name)
+    );
   } catch (error) {
     console.error(error);
   }
 };
 
 const correctUrl = "https://pokeapi.co/api/v2/pokemon/2/";
-// visualize(5);
+visualize(5);
 visualize(new Request(correctUrl));
-// visualize(correctUrl);
+visualize(correctUrl);
 
 const wrongUrl = "https://pokeapi.co/api/v2/pokemon/999/";
 visualize(new Request(wrongUrl));
-// visualize(wrongUrl);
+visualize(wrongUrl);
