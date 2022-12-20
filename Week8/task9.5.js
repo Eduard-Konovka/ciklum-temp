97d5f54b369cb64a0f16e1d2b8c4e25bf4e0b4c7
Eduard Konovka <ed098ua@gmail.com>
2022-12-20 19:14:32 +0200

    Adds solution of task9.5

--- Week8/task9.5.js
+++ Week8/task9.5.js
@@ -1,41 +1,32 @@
 async function handleRequest(options) {
   // ========== Початок зони редагування ==============
-  let correctType = typeof options === "object" || typeof options === "string";
+  const correctType =
+    typeof options === "object" || typeof options === "string";
   if (!correctType) {
     throw new Error("Expecting string or object as fetch parameter");
   }
 
-  return fetch(options).then((response) => {
-    // варіант #1 -------------------------------------
-    if (response.status >= 200 && response.status < 400) {
-      return Promise.resolve(response.json());
-    } else if (response.status >= 400) {
-      throw new Error(response.status);
-    }
+  const result = await fetch(options);
 
-    // варіант #2 -------------------------------------
-    return new Promise((resolve, reject) => {
-      if (response.status >= 200 && response.status < 400) {
-        resolve(response.json());
-      } else if (response.status >= 400) {
-        reject(new Error(response.status));
-        // варіант #2.2 -------------------------------
-        throw new Error(response.status);
-      }
-    });
-  });
+  if (result.status >= 200 && result.status < 400) {
+    return result;
+  } else {
+    throw new Error(result.status);
+  }
   // ========== Кінець зони редагування ===============
 }
 
-// Код для перевірки в консолі =======================
+// Код для перевірки в консолі ========================
 const visualize = async (val) => {
   try {
     console.log(
       "Name of pokemon ---> ",
-      await handleRequest(val).then((obj) => obj.name)
+      await handleRequest(val)
+        .then((data) => data.json())
+        .then((obj) => obj.name)
     );
   } catch (error) {
-    console.error(error);
+    console.error("Error ---> ", error.message);
   }
 };
 
