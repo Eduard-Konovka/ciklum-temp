fafd47da77994aa0cf1556cb2ccc6031ca46990f
Eduard Konovka <ed098ua@gmail.com>
2023-01-14 00:39:56 +0200

    Adds solution of task13.3

--- Week11/task13.3/components/Callback.js
+++ Week11/task13.3/components/Callback.js
@@ -1,36 +1,46 @@
-import './callback.css';
+import { useState, useCallback } from "react";
+import "./callback.css";
 
 const Callback = () => {
+  const [number, setNumber] = useState(1);
+  const [year, setYear] = useState(2021);
+  const [secretNumber, setSecretNumber] = useState(number * year * 3);
+
+  const changeSecretNumberHandler = useCallback(
+    (num) => setSecretNumber(number * year * num),
+    [year]
+  );
+
   return (
     <div>
       <h2>Callback component</h2>
       <ol className="list-group">
         <li className="callback__year list-group-item">
-          Поточний рік: <span>year</span>
+          Поточний рік: <span>{year}</span>
         </li>
         <li className="callback__number list-group-item">
-          Поточне число: <span>number</span>
+          Поточне число: <span>{number}</span>
         </li>
         <li className="callback__secret-number list-group-item">
-          Поточне секретне число: <span>secretNumber</span>
+          Поточне секретне число: <span>{secretNumber}</span>
         </li>
       </ol>
       <div className="callback__buttons">
         <button
           className="callback__button-year btn btn-primary"
-          onClick={() => {}}
+          onClick={() => setYear(year + 1)}
         >
           Змінити рік
         </button>
         <button
           className="callback__button-number btn btn-secondary"
-          onClick={() => {}}
+          onClick={() => setNumber(number + 1)}
         >
           Змінити число
         </button>
         <button
           className="callback__button-seacret-number btn btn-success"
-          onClick={() => {}}
+          onClick={() => changeSecretNumberHandler(3)}
         >
           Змінити секретне число
         </button>
--- Week11/task13.3/components/Memo.js
+++ Week11/task13.3/components/Memo.js
@@ -1,32 +1,42 @@
-import './memo.css';
+import { useState, useEffect, useMemo } from "react";
+import "./memo.css";
 
 const Memo = () => {
+  const [number, setNumber] = useState(4);
+  const [year, setYear] = useState(2021);
+  const [secretNumber, setSecretNumber] = useState(null);
+
+  // Однаковий результат з useMemo:
+  useMemo(() => setSecretNumber(number * year * 10), [year]);
+  // або з useEffect:
+  useEffect(() => setSecretNumber(number * year * 10), [year]);
+
   return (
     <div>
       <h3>Memo</h3>
       <ol className="list-group">
         <li className="memo__year list-group-item">
-          Поточний рік: <span>year</span>
+          Поточний рік: <span>{year}</span>
         </li>
         <li className="memo__number list-group-item">
-          Поточне число: <span>number</span>
+          Поточне число: <span>{number}</span>
         </li>
         <li className="memo__secret-number list-group-item">
-          Поточне секретне число: <span>secretNumber</span>
+          Поточне секретне число: <span>{secretNumber}</span>
         </li>
       </ol>
       <div className="memo__buttons">
         <button
           type="button"
           className="memo__button-year btn btn-primary"
-          onClick={() => {}}
+          onClick={() => setYear(year + 1)}
         >
           Змінити рік
         </button>
         <button
           type="button"
           className="memo__button-number btn btn-secondary"
-          onClick={() => {}}
+          onClick={() => setNumber(number + 1)}
         >
           Змінити число
         </button>
