8ad26ba2c6f132e980f8e17a6233cddeb0c6acd1
Eduard Konovka <ed098ua@gmail.com>
2023-01-25 17:20:55 +0200

    Adds 15.1

---
+++ Week12/15.1/test.js
@@ -0,0 +1,23 @@
+let assert = require('assert');
+
+function multiply(a, b) {
+    return a * b;
+}
+
+describe('multiply positive', function() {
+    it("multiply 2 on 3", function() {
+        assert.equal(multiply(2, 3), 6);
+    });
+    it("multiply 4 on 4", function() {
+        assert.equal(multiply(4, 4), 16);
+    });
+});
+
+describe('multiply negative', function() {
+    it("multiply 2 on a", function() {
+        assert.equal(multiply(2, "a"), NaN);
+    });
+    it("multiply a", function() {
+        assert.equal(multiply(2), NaN);
+    });
+})
